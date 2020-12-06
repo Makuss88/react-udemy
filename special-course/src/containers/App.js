a0a635763aa983628728b54a8f05e0f387c24e03
Makuss88 <maciborek.grzegorz@gmail.com>
2020-12-06 17:56:41 +0100

    next spet in course - build app MyBurger

--- special-course/src/containers/App.js
+++ special-course/src/containers/App.js
@@ -1,88 +1,12 @@
-import React, { useState } from 'react';
-// import Radium, { StyleRoot } from 'radium';
-
-import Cockpit from '../Components/Cockpit/Cockpit';
-import Persons from '../Components/Persons/Persons';
+import React from 'react';
 
 import './App.css';
 
 const App = () => {
-  const [personState, setPersonState] = useState({
-    persons: [
-      { id:'dasj', name: "Adam", age: 25 },
-      { id:'adfa', name: "Grzes", age: 33 },
-      { id:'asff', name: "Drosofila", age: 18},
-    ]
-  });
-  const [showPersons, setShowPerson] = useState(false)
-  
-
-  const handleShowPersons = () => {
-    setShowPerson(!showPersons);   
-  }
-
-  const handleChangePersonDetails = (event, id) => {
-    const personIndex = personState.persons.findIndex(p => {
-      return p.id === id
-    });
-
-    const person = {...personState.persons[personIndex]};
-    person.name = event.target.value;
-
-    const persons = [...personState.persons];
-    persons[personIndex] = person; 
-    setPersonState({persons: persons});
-  }
-
-  const deletePerson = (personIndex) => {
-    const person = [...personState.persons];
-    person.splice(personIndex, 1);
-    setPersonState({persons: person});
-  }
-
-  // const style = {
-  //   backgroundColor: 'green',
-  //   width: '200px',
-  //   height: '40px',
-  //   color: 'white',
-  //   ':hover': {
-  //     backgroundColor: 'lightgreen',
-  //     color: 'black',  
-  //   },
-  // }
-
-  // if(persons) {
-  //   style.backgroundColor = 'red'
-  //   style[':hover'] = {
-  //     backgroundColor: 'tomato',
-  //     color: 'black',  
-  //   }
-  // }
-
-  let persons = null;
-
-  if (showPersons) {
-    persons = (
-      <div>
-        <div>
-          <Persons 
-            persons={personState.persons}
-            clicked={deletePerson}
-            changed={handleChangePersonDetails}/>
-        </div>
-      </div>
-    );
-  }
 
   return (
-    <div className="App">
-      <header>
-        <Cockpit 
-          alt={showPersons} 
-          click={handleShowPersons}
-          persons={personState.persons}/>
-        {persons}
-      </header>
+    <div>
+
     </div>
   );
  
--- special-course/src/index.js
+++ special-course/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from './containers/App3';
 // import App2 from './containers/App2';
+// import Ap32 from './containers/App3';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -12,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
