c344972765167f028d70d724a00c054c047c897d
Makuss88 <maciborek.grzegorz@gmail.com>
2020-12-20 15:37:13 +0100

    first file about redux, install redux, file redux-basic.js to understand

---
+++ special-course/redux-basic.js
@@ -0,0 +1,38 @@
+const redux = require('redux');
+
+const createStroe = redux.createStore;
+
+const initialState = {
+  counter: 0
+}
+
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    }
+  } else if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
+  return state;
+}
+
+const store = createStroe(rootReducer);
+console.log(store.getState());
+
+store.subscribe(() => {
+  console.log('sub', store.getState())
+})
+
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 })
+
+console.log(store.getState());
+
+// store.subscribe(() => {
+//   console.log('sub', store.getState())
+// })
