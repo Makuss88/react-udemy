7a45f1e8bc4bfe58ffc58d53eedbde2d5af0e14b
Makuss88 <maciborek.grzegorz@gmail.com>
2020-12-30 14:22:17 +0100

    auth my Appication, first step -> build navigation and actions in store

--- special-course/src/App.js
+++ special-course/src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './Containers/Checkout/Checkout';
 import Orders from './Containers/Orders/Orders'
+import Auth from './Components/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
